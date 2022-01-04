64514d39f88c37718d21baa0e08e39c6f962ddd6
tishunator <tishubezarius@mail.ru>
2022-01-04 17:21:00 +0300

    Дописала на 768

--- scripts/main.js
+++ scripts/main.js
@@ -15,26 +15,26 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
-$('#click').click(function(){ $('#desc1').show(); $('#click').hide(); });
+$('#click').click(function(){ $('#desc1').show();$("#img1").show(); $('#click').hide(); });
 $('#click').click(function(){ $('#clickReverse').show();});
 $('#clickReverse').click(function(){ $('#desc1').hide();});
-$('#clickReverse').click(function(){ $('#desc1').hide(); $('#clickReverse').hide(); $('#click').show();  });
+$('#clickReverse').click(function(){ $('#desc1').hide();$('#img1').hide(); $('#clickReverse').hide(); $('#click').show();  });
 
 
 
-$('#click2').click(function(){ $('#desc2').show(); $('#click2').hide();  });
+$('#click2').click(function(){ $('#desc2').show();$("#img2").show(); $('#click2').hide();  });
 $('#click2').click(function(){ $('#clickReverse2').show();});
 $('#clickReverse2').click(function(){ $('#desc2').hide();});
-$('#clickReverse2').click(function(){ $('#desc2').hide(); $('#clickReverse2').hide(); $('#click2').show();  });
+$('#clickReverse2').click(function(){ $('#desc2').hide();$('#img2').hide(); $('#clickReverse2').hide(); $('#click2').show();  });
 
-$('#click3').click(function(){ $('#desc3').show(); $('#click3').hide();  });
+$('#click3').click(function(){ $('#desc3').show();$("#img3").show(); $('#click3').hide();  });
 $('#click3').click(function(){ $('#clickReverse3').show();});
 $('#clickReverse3').click(function(){ $('#desc3').hide();});
-$('#clickReverse3').click(function(){ $('#desc3').hide(); $('#clickReverse3').hide(); $('#click3').show();  });
+$('#clickReverse3').click(function(){ $('#desc3').hide();$('#img3').hide(); $('#clickReverse3').hide(); $('#click3').show();  });
 
-$('#click4').click(function(){ $('#desc4').show(); $('#click4').hide();  });
+$('#click4').click(function(){ $('#desc4').show();$("#img4").show(); $('#click4').hide();  });
 $('#click4').click(function(){ $('#clickReverse4').show();});
 $('#clickReverse4').click(function(){ $('#desc4').hide();});
-$('#clickReverse4').click(function(){ $('#desc4').hide(); $('#clickReverse4').hide(); $('#click4').show();  });
+$('#clickReverse4').click(function(){ $('#desc4').hide();$('#img4').hide();/*$('#img4').hide(); вот эта строчка прячет картинку*/ $('#clickReverse4').hide(); $('#click4').show();  });
 
 
