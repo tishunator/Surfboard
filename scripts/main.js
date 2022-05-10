460d7400781cd5f4693bb57f15ee2b87b15d6e37
tishunator <tishubezarius@mail.ru>
2022-05-10 15:23:33 +0300

    Изменила js в team

--- scripts/main.js
+++ scripts/main.js
@@ -15,27 +15,23 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
-$('#click').click(function(){ $('#desc1').show();$("#img1").show(); $('#click').hide(); });
-$('#click').click(function(){ $('#clickReverse').show();});
-$('#clickReverse').click(function(){ $('#desc1').hide();});
-$('#clickReverse').click(function(){ $('#desc1').hide();$('#img1').hide(); $('#clickReverse').hide(); $('#click').show();  });
+const details = document.querySelectorAll('[detail]');
 
-
-
-$('#click2').click(function(){ $('#desc2').show();$("#img2").show(); $('#click2').hide();  });
-$('#click2').click(function(){ $('#clickReverse2').show();});
-$('#clickReverse2').click(function(){ $('#desc2').hide();});
-$('#clickReverse2').click(function(){ $('#desc2').hide();$('#img2').hide(); $('#clickReverse2').hide(); $('#click2').show();  });
-
-$('#click3').click(function(){ $('#desc3').show();$("#img3").show(); $('#click3').hide();  });
-$('#click3').click(function(){ $('#clickReverse3').show();});
-$('#clickReverse3').click(function(){ $('#desc3').hide();});
-$('#clickReverse3').click(function(){ $('#desc3').hide();$('#img3').hide(); $('#clickReverse3').hide(); $('#click3').show();  });
-
-$('#click4').click(function(){ $('#desc4').show();$("#img4").show(); $('#click4').hide();  });
-$('#click4').click(function(){ $('#clickReverse4').show();});
-$('#clickReverse4').click(function(){ $('#desc4').hide();});
-$('#clickReverse4').click(function(){ $('#desc4').hide();$('#img4').hide();/*$('#img4').hide(); вот эта строчка прячет картинку*/ $('#clickReverse4').hide(); $('#click4').show();  });
+details.forEach(item => {
+    item.addEventListener('click', function() {
+        details.forEach(item => {
+            item.querySelectorAll('[detail-content]').forEach(item =>{
+                item.style.display = "none"
+            })
+           
+        })
+        const dContent = item.querySelectorAll('[detail-content]')
+        dContent.forEach(item=>{
+            item.style.display = "block";
+        })
+       
+    })
+})
 
 $(document).ready(function() {
 	$('.menu-burger__header').click(function(){
